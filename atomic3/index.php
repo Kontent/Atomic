c6adfd64d5e57e49a16debda9c9686f04808e9c9
Ron Severdia <severdia@gmail.com>
2017-10-12 17:40:17 -0700

    Atomic version 3.1
    Fixes for FA, CSS, add custom CSS in config, fix abovebody module

--- atomic3/index.php
+++ atomic3/index.php
@@ -16,7 +16,7 @@ $this->setHtml5(true);
 // Getting params from template
 $params = $app->getTemplate(true)->params;
 
-// Get params
+// Assign params
 $sitetitle								= $this->params->get('sitetitle');
 $sitedescription					= $this->params->get('sitedescription');
 $bootstraplocal					= $this->params->get('bootstraplocal');
@@ -25,12 +25,13 @@ $bootstrapcdn						= $this->params->get('bootstrapcdn');
 $bootswatch						= $this->params->get('bootswatch');
 $fontawesome						= $this->params->get('fontawesome');
 $elegantfont						= $this->params->get('elegantfont');
-$customcss							= $this->params->get('customcss');
+$customcsscode					= $this->params->get('customcsscode');
+$customcssfile					= $this->params->get('customcssfile');
 $customjs							= $this->params->get('customjs');
 $fluidcontainer						= $this->params->get('fluidcontainer');
 $jqlibrary								= $this->params->get('jqlibrary');
 $jquerycdn							= $this->params->get('jquerycdn');
-$bs3joomla							= $this->params->get('bs3joomla');
+$bsfixjoomla						= $this->params->get('bsfixjoomla');
 $gacode								= $this->params->get('gacode');
 $pageheader						= $this->params->get('pageheader');
 $topmenu							= $this->params->get('topmenu');
@@ -87,6 +88,8 @@ $copyright							= $this->params->get('copyright');
 		<?php elseif(($bootstrapcdn == null) && ($bootstraplocal == 1)) : ?>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<?php elseif(($bootstrapcdn == null) && ($bootstraplocal == 3)) : ?>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<?php else : ?>
 			<?php echo $bootstrapcdn ?>
 		<?php endif; ?>
@@ -95,48 +98,52 @@ $copyright							= $this->params->get('copyright');
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/bootstrap-theme.min.css" type="text/css" />
 		<?php endif; ?>		
 			
-		<?php if($bootswatch == 'cerulean') : ?>
+		<?php if(($bootswatch == 'cerulean') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-zF4BRsG/fLiTGfR9QL82DrilZxrwgY/+du4p/c7J72zZj+FLYq4zY00RylP9ZjiT" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'cosmo') : ?>
+		<?php elseif(($bootswatch == 'cosmo') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha384-h21C2fcDk/eFsW9sC9h0dhokq5pDinLNklTKoxIZRUn3+hvmgQSffLLQ4G4l2eEr" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'cyborg') : ?>
+		<?php elseif(($bootswatch == 'cyborg') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cyborg/bootstrap.min.css" rel="stylesheet" integrity="sha384-D9XILkoivXN+bcvB2kSOowkIvIcBbNdoDQvfBNsxYAIieZbx8/SI4NeUvrRGCpDi" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'darkly') : ?>
+		<?php elseif(($bootswatch == 'darkly') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-S7YMK1xjUjSpEnF4P8hPUcgjXYLZKK3fQW1j5ObLSl787II9p8RO9XUGehRmKsxd" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'flatly') : ?>
+		<?php elseif(($bootswatch == 'flatly') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'journal') : ?>
+		<?php elseif(($bootswatch == 'journal') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/journal/bootstrap.min.css" rel="stylesheet" integrity="sha384-1L94saFXWAvEw88RkpRz8r28eQMvt7kG9ux3DdCqya/P3CfLNtgqzMnyaUa49Pl2" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'lumen') : ?>
+		<?php elseif(($bootswatch == 'lumen') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-gv0oNvwnqzF6ULI9TVsSmnULNb3zasNysvWwfT/s4l8k5I+g6oFz9dye0wg3rQ2Q" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'paper') : ?>
+		<?php elseif(($bootswatch == 'paper') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css" rel="stylesheet" integrity="sha384-awusxf8AUojygHf2+joICySzB780jVvQaVCAt1clU3QsyAitLGul28Qxb2r1e5g+" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'readable') : ?>
+		<?php elseif(($bootswatch == 'readable') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-Li5uVfY2bSkD3WQyiHX8tJd0aMF91rMrQP5aAewFkHkVSTT2TmD2PehZeMmm7aiL" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'sandstone') : ?>
+		<?php elseif(($bootswatch == 'sandstone') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/sandstone/bootstrap.min.css" rel="stylesheet" integrity="sha384-G3G7OsJCbOk1USkOY4RfeX1z27YaWrZ1YuaQ5tbuawed9IoreRDpWpTkZLXQfPm3" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'simplex') : ?>
+		<?php elseif(($bootswatch == 'simplex') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/simplex/bootstrap.min.css" rel="stylesheet" integrity="sha384-C0X5qw1DlkeV0RDunhmi4cUBUkPDTvUqzElcNWm1NI2T4k8tKMZ+wRPQOhZfSJ9N" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'slate') : ?>
+		<?php elseif(($bootswatch == 'slate') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/slate/bootstrap.min.css" rel="stylesheet" integrity="sha384-RpX8okQqCyUNG7PlOYNybyJXYTtGQH+7rIKiVvg1DLg6jahLEk47VvpUyS+E2/uJ" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'solar') : ?>
+		<?php elseif(($bootswatch == 'solar') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/solar/bootstrap.min.css" rel="stylesheet" integrity="sha384-GC77SCz5O11gVtXl0sSfbQYEWSSznn1wPDHgL1BGUTFU9iEoUrG4IOJa5CBVY8kR" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'spacelab') : ?>
+		<?php elseif(($bootswatch == 'spacelab') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/spacelab/bootstrap.min.css" rel="stylesheet" integrity="sha384-L/tgI3wSsbb3f/nW9V6Yqlaw3Gj7mpE56LWrhew/c8MIhAYWZ/FNirA64AVkB5pI" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'superhero') : ?>
+		<?php elseif(($bootswatch == 'superhero') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css" rel="stylesheet" integrity="sha384-Xqcy5ttufkC3rBa8EdiAyA1VgOGrmel2Y+wxm4K3kI3fcjTWlDWrlnxyD6hOi3PF" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'united') : ?>
+		<?php elseif(($bootswatch == 'united') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css" rel="stylesheet" integrity="sha384-pVJelSCJ58Og1XDc2E95RVYHZDPb9AVyXsI8NoVpB2xmtxoZKJePbMfE4mlXw7BJ" crossorigin="anonymous">
-		<?php elseif($bootswatch == 'yeti') : ?>
+		<?php elseif(($bootswatch == 'yeti') && ($bootstraplocaltheme == 1)) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet" integrity="sha384-HzUaiJdCTIY/RL2vDPRGdEQHHahjzwoJJzGUkYjHVzTwXFQ2QN/nVgX7tzoMW3Ov" crossorigin="anonymous">
 		<?php endif; ?>		
 		
-		<?php if($bs3joomla == 1) : ?>
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template_bs3.css" type="text/css" />
+		<?php if($bsfixjoomla == 1) : ?>
+			<?php if(($bootstraplocal == 0) || ($bootstraplocal == 1)) : ?>
+				<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template_bs3.css" type="text/css" />
+			<?php elseif($bootstraplocal == 3) : ?>
+				<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template_bs4.css" type="text/css" />
+			<?php endif; ?>
 		<?php endif; ?>
 		
 		<?php if($fontawesome == 1) : ?>
-			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/bootstrap.min.css" type="text/css" />
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/font-awesome.min.css" type="text/css" />
 		<?php elseif($fontawesome == 2) : ?>
 			<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 		<?php endif; ?>
@@ -149,8 +156,14 @@ $copyright							= $this->params->get('copyright');
 			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template_rtl.css" type="text/css" />
 		<?php endif; ?>
 		
-		<?php if($customcss == 1) : ?>
-			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+		<?php if($customcssfile == 1) : ?>
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template_custom.css" type="text/css" />
+		<?php endif; ?>
+		
+		<?php if($customcsscode != null) : ?>
+			<style>
+				<?php echo $customcsscode ?>
+			</style>
 		<?php endif; ?>
 				
 		<?php if(($headerfont == 1) && ($bodyfont == 1) && ($headerfontname != null) && ($bodyfontname != null)) : ?>
@@ -187,6 +200,7 @@ $copyright							= $this->params->get('copyright');
 			</div>
 		</div>
 		<?php endif; ?>
+		
 		<?php if($topmenu == 1) : ?>
 		<div class="row">
 			<div class="col-md-9">
@@ -210,6 +224,9 @@ $copyright							= $this->params->get('copyright');
  			</div>
  			<div class="col-md-8">
  				<jdoc:include type="message" />
+ 				<?php if($abovebody == 1) : ?>
+				<jdoc:include type="modules" name="abovebody" />
+				<?php endif; ?>
 				<jdoc:include type="component" />
 				<?php if($belowbody == 1) : ?>
 				<jdoc:include type="modules" name="belowbody" />
@@ -222,6 +239,9 @@ $copyright							= $this->params->get('copyright');
 			<?php elseif(($leftbody == 0) && ($rightbody == 1)) : ?>
  			<div class="col-md-10">
  				<jdoc:include type="message" />
+ 				<?php if($abovebody == 1) : ?>
+				<jdoc:include type="modules" name="abovebody" />
+				<?php endif; ?>
 				<jdoc:include type="component" />
 				<?php if($belowbody == 1) : ?>
 				<jdoc:include type="modules" name="belowbody" />
@@ -237,6 +257,9 @@ $copyright							= $this->params->get('copyright');
  			</div>
  			<div class="col-md-10">
  				<jdoc:include type="message" />
+ 				<?php if($abovebody == 1) : ?>
+				<jdoc:include type="modules" name="abovebody" />
+				<?php endif; ?>
 				<jdoc:include type="component" />
 				<?php if($belowbody == 1) : ?>
 				<jdoc:include type="modules" name="belowbody" />
@@ -246,6 +269,9 @@ $copyright							= $this->params->get('copyright');
 			<?php else : ?>
  			<div class="col-md-12">
  				<jdoc:include type="message" />
+ 				<?php if($abovebody == 1) : ?>
+				<jdoc:include type="modules" name="abovebody" />
+				<?php endif; ?>
 				<jdoc:include type="component" />
 				<?php if($belowbody == 1) : ?>
 				<jdoc:include type="modules" name="belowbody" />
