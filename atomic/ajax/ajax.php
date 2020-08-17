bac092a8e79c2a1f11bdce193a28b5c17d370259
severdia <severdia@gmail.com>
2020-08-17 11:45:06 -0700

    Update CSS path for AJAX creation

--- atomic/ajax/ajax.php
+++ atomic/ajax/ajax.php
@@ -7,12 +7,12 @@
 if (isset($_POST["id"]))  {
 	$path =  $_SERVER["PHP_SELF"];
 	$path = substr($path, 0, strpos($path, "templates"));
-	if (file_exists($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic4/css/template.css")) {
+	if (file_exists($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic/css/template.css")) {
 		echo '0';
 	}
 	else {
 		$content = "/* Add your custom CSS here. */";
-		$fp = fopen($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic4/css/template.css","wb") or die('File already exists!');
+		$fp = fopen($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic/css/template.css","wb") or die('File already exists!');
 		echo "1";
 		fwrite($fp,$content);
 		fclose($fp);
@@ -22,7 +22,7 @@ if (isset($_POST["id"]))  {
 if (isset($_POST["script_id"])) {
 	$path =  $_SERVER["PHP_SELF"];
 	$path = substr($path, 0, strpos($path, "templates"));
-	if (file_exists($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic4/css/template.css")) {
+	if (file_exists($_SERVER['DOCUMENT_ROOT'] . $path."templates/atomic/css/template.css")) {
 		echo '2';
 	}
 }
