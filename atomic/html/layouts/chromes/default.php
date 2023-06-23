c8b577ea1f215a29b584c5fe75f05a5711130750
severdia <severdia@gmail.com>
2023-06-23 14:50:09 -0400

    Module chrome updates

--- atomic/html/layouts/chromes/default.php
+++ atomic/html/layouts/chromes/default.php
@@ -1,34 +1,44 @@
 <?php
+
 /**
- * @copyright   Copyright (C) 2005 - 2020 Ron Severdia All rights reserved.
+ * @package     Joomla.Site
+ * @subpackage  Templates.cassiopeia
+ *
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
+use Joomla\Utilities\ArrayHelper;
+
 $module  = $displayData['module'];
 $params  = $displayData['params'];
 $attribs = $displayData['attribs'];
 
-if ($module->content === null || $module->content === '')
-{
-	return;
+if ($module->content === null || $module->content === '') {
+    return;
 }
 
-$modulePos   = $module->position;
-$moduleTag   = $params->get('module_tag', 'div');
-$headerTag   = htmlspecialchars($params->get('header_tag', 'h4'), ENT_QUOTES, 'UTF-8');
-$headerClass = htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
+$moduleTag              = $params->get('module_tag', 'div');
+$moduleAttribs          = [];
+$moduleAttribs['class'] = $module->position . htmlspecialchars($params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$headerTag              = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
+$headerClass            = htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
+$headerAttribs          = [];
+$headerAttribs['class'] = $headerClass;
 
+
+$header = '<' . $headerTag . ' ' . ArrayHelper::toString($headerAttribs) . '>' . $module->title . '</' . $headerTag . '>';
 ?>
-<<?php echo $moduleTag; ?> class="<?php echo $modulePos; ?> module <?php echo htmlspecialchars($params->get('moduleclass_sfx'), ENT_QUOTES, 'UTF-8'); ?>">
-	<?php if ($module->showtitle && $headerClass !== 'card-title') : ?>
-		<<?php echo $headerTag; ?> class="card-header <?php echo $headerClass; ?>"><?php echo $module->title; ?></<?php echo $headerTag; ?>>
-	<?php endif; ?>
-	<div class="card-body">
-		<?php if ($module->showtitle && $headerClass === 'card-title') : ?>
-			<<?php echo $headerTag; ?> class="<?php echo $headerClass; ?>"><?php echo $module->title; ?></<?php echo $headerTag; ?>>
-		<?php endif; ?>
-		<?php echo $module->content; ?>
-	</div>
+<<?php echo $moduleTag; ?> <?php echo ArrayHelper::toString($moduleAttribs); ?>>
+    <?php if ($module->showtitle) : ?>
+        <?php echo $header; ?>
+    <?php endif; ?>
+    <div class="module-body">
+        <?php if ($module->showtitle) : ?>
+            <?php echo $header; ?>
+        <?php endif; ?>
+        <?php echo $module->content; ?>
+    </div>
 </<?php echo $moduleTag; ?>>
---
+++ atomic/html/mod_custom/modulestyle.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+// use Joomla\CMS\Helper\ModuleHelper;
+
+$modId = 'mod-custom' . $module->id;
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx', ''), ENT_COMPAT, 'UTF-8');
+
+if ($params->get('backgroundimage')) {
+    /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+    $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+    $wa->addInlineStyle('
+#' . $modId . '{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}
+', ['name' => $modId]);
+}
+
+?>
+
+<div id="<?php echo $modId; ?>" class="mod-custom custom <?php echo $moduleclass_sfx; ?>">
+    <?php echo $module->content; ?>
+</div>
