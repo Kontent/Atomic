697fb2295d11063f85b394dcbbe23960508ffacc
severdia <severdia@gmail.com>
2023-06-26 07:45:16 -0400

    Bootstrap menu overrides (vertical & horizontal)

---
+++ atomic/html/mod_menu/vertical.php
@@ -0,0 +1,74 @@
+<?php
+defined('_JEXEC') or die;
+
+$tagId = $params->get('tag_id', '');
+$standard = ['separator', 'component', 'heading', 'url'];
+$startLevel = $params->get('startLevel');
+$endLevel = $params->get('endLevel');
+?>
+
+<div class="nav vertical menu<?php echo $class_sfx; ?>" <?php echo $tagId ? 'id="' . $tagId . '"' : '';?> id="navigation">
+	
+	<?php foreach ($list as $i => &$item) { ?>
+		<?php
+    $itemParams = $item->getParams();
+			$class = 'item-' . $item->id;
+
+			if ($item->id == $default_id) {
+				$class .= ' default';
+			}
+
+			if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id)) {
+				$class .= ' current';
+			}
+
+			if (in_array($item->id, $path)) {
+				$class .= ' active';
+			} elseif ($item->type === 'alias') {
+				$aliasToId = $itemParams->get('aliasoptions');
+
+				if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+					$class .= ' active';
+				}
+				elseif (in_array($aliasToId, $path))
+				{
+					$class .= ' alias-parent-active';
+				}
+			}
+
+			if ($item->type === 'separator') {
+				$class .= ' divider';
+			}
+
+			if ($item->deeper) {
+				$class .= ' deeper';
+			}
+
+			if ($item->parent) {
+				$class .= ' parent';
+			}
+		?>
+		
+		<?php if ($item->deeper) { ?>
+			<div class="btn-group dropend">
+		<?php } ?>
+
+			<?php if (in_array($item->type, $standard)) { ?>
+				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'vertical_' . $item->type)); ?>
+			<?php } else { ?>
+				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'vertical_url')); ?>
+			<?php } ?>
+
+			<?php if ($item->deeper) { ?>
+				<ul class="dropdown-menu">
+			<?php } ?>
+
+			<?php if ($item->shallower) { ?>
+				</ul>	
+			</div>
+			
+			<?php } ?>
+	<?php } ?>
+		
+</div>
+
---
+++ atomic/html/mod_menu/vertical_url.php
@@ -0,0 +1,79 @@
+<?php
+defined('_JEXEC') or die;
+
+$attributes = array();
+
+if ($item->anchor_title) {
+	$attributes['title'] = $item->anchor_title;
+}
+
+$attributes['class'] = '';
+
+if ($item->parent && $item->level <= $startLevel) {
+	$attributes['class'] .= 'btn dropdown-toggle';
+	$attributes['data-bs-toggle'] = 'dropdown';
+	//$attributes['aria-haspopup'] = 'true';
+	$attributes['aria-expanded'] = 'false';
+
+	// If this is a parent link, we need to ensure that the link goes nowhere
+	$item->flink = '#';
+}
+
+if ($item->level > $startLevel) {
+	$attributes['class'] = 'dropdown-item';
+}
+
+$attributes['class'] .= ' ' . $class;
+
+// Options from menu
+if ($item->anchor_css) {
+	$attributes['class'] .= ' ' . $item->anchor_css;
+}
+
+if ($item->anchor_rel) {
+	$attributes['rel'] = $item->anchor_rel;
+}
+
+$linktype = $item->title;
+
+if ($item->menu_image) {
+	if ($item->menu_image_css) {
+		$image_attributes['class'] = $item->menu_image_css;
+		$linktype = JHtml::_('image', $item->menu_image, $item->title, $image_attributes);
+	} else {
+		$linktype = JHtml::_('image', $item->menu_image, $item->title);
+	}
+
+	if ($item->params->get('menu_text', 1)) {
+		$linktype .= '<span class="image-title">' . $item->title . '</span>';
+	}
+}
+
+if ($item->browserNav == 1) {
+	$attributes['target'] = '_blank';
+	$attributes['rel'] = 'noopener noreferrer';
+
+	if ($item->anchor_rel == 'nofollow') {
+		$attributes['rel'] .= ' nofollow';
+	}
+} elseif ($item->browserNav == 2) {
+	$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
+
+	$attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
+}
+if ($item->parent && $item->level <= $startLevel) {
+    echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
+}
+else
+{
+    
+   if($item->level=='1')
+   {
+  $attributes['class']='first-heading';
+    echo "<div class='btn-group'>".JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes)."</div>";
+   }
+  else
+  {
+    echo "<li>".JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes)."</li>";
+  }
+}
