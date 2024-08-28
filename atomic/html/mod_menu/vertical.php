5e9045a1af27d74553560696bef505987dceccb2
locjmz <52687099+locjmz@users.noreply.github.com>
2024-08-29 01:36:38 +0700

    Fix mod_menu layout vertical display style, add responsive image

--- atomic/html/mod_menu/vertical.php
+++ atomic/html/mod_menu/vertical.php
@@ -9,7 +9,7 @@ $startLevel = $params->get('startLevel');
 $endLevel = $params->get('endLevel');
 
 $attributes          = [];
-$attributes['class'] = 'nav menu vertical' . $class_sfx;
+$attributes['class'] = 'nav menu vertical flex-column' . $class_sfx;
 
 if ($tagId = $params->get('tag_id', '')) {
     $attributes['id'] = $tagId;
@@ -17,68 +17,68 @@ if ($tagId = $params->get('tag_id', '')) {
 
 ?>
 
-<div <?php echo ArrayHelper::toString($attributes); ?>>
-	
-	<?php foreach ($list as $i => &$item) { ?>
-		<?php
+<ul <?php echo ArrayHelper::toString($attributes); ?>>
+<?php foreach ($list as $i => &$item) {
     $itemParams = $item->getParams();
-			$class = 'item-' . $item->id;
+    $class      = 'nav-item item-' . $item->id;
 
-			if ($item->id == $default_id) {
-				$class .= ' default';
-			}
+    if ($item->id == $default_id) {
+        $class .= ' default';
+    }
 
-			if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id)) {
-				$class .= ' current';
-			}
+    if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id)) {
+        $class .= ' current';
+    }
 
-			if (in_array($item->id, $path)) {
-				$class .= ' active';
-			} elseif ($item->type === 'alias') {
-				$aliasToId = $itemParams->get('aliasoptions');
+    if (in_array($item->id, $path)) {
+        $class .= ' active';
+    } elseif ($item->type === 'alias') {
+        $aliasToId = $itemParams->get('aliasoptions');
 
-				if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
-					$class .= ' active';
-				}
-				elseif (in_array($aliasToId, $path))
-				{
-					$class .= ' alias-parent-active';
-				}
-			}
+        if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+            $class .= ' active';
+        } elseif (in_array($aliasToId, $path)) {
+            $class .= ' alias-parent-active';
+        }
+    }
 
-			if ($item->type === 'separator') {
-				$class .= ' divider';
-			}
+    if ($item->type === 'separator') {
+        $class .= ' divider';
+    }
 
-			if ($item->deeper) {
-				$class .= ' deeper';
-			}
+    if ($item->deeper) {
+        $class .= ' deeper';
+    }
 
-			if ($item->parent) {
-				$class .= ' parent';
-			}
-		?>
-		
-		<?php if ($item->deeper) { ?>
-			<div class="btn-group dropend">
-		<?php } ?>
+    if ($item->parent) {
+        $class .= ' parent';
+    }
 
-			<?php if (in_array($item->type, $standard)) { ?>
-				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'vertical_' . $item->type)); ?>
-			<?php } else { ?>
-				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'vertical_url')); ?>
-			<?php } ?>
+    echo '<li class="' . $class . '">';
 
-			<?php if ($item->deeper) { ?>
-				<ul class="dropdown-menu">
-			<?php } ?>
+    switch ($item->type) :
+        case 'separator':
+        case 'component':
+        case 'heading':
+        case 'url':
+            require ModuleHelper::getLayoutPath('mod_menu', 'horizontal_' . $item->type);
+            break;
 
-			<?php if ($item->shallower) { ?>
-				</ul>	
-			</div>
-			
-			<?php } ?>
-	<?php } ?>
-		
-</div>
+        default:
+            require ModuleHelper::getLayoutPath('mod_menu', 'horizontal_url');
+            break;
+    endswitch;
 
+    // The next item is deeper.
+    if ($item->deeper) {
+        echo '<ul class="dropdown-menu">';
+    } elseif ($item->shallower) {
+        // The next item is shallower.
+        echo '</li>';
+        echo str_repeat('</ul></li>', $item->level_diff);
+    } else {
+        // The next item is on the same level.
+        echo '</li>';
+    }
+}
+?></ul>
