9fcdf35bf6ae2d2414d4435954e7e297b54ded48
locjmz <52687099+locjmz@users.noreply.github.com>
2024-08-29 01:07:33 +0700

    Fix mod_menu layout horizontaltabs display style

--- atomic/html/mod_menu/horizontaltabs.php
+++ atomic/html/mod_menu/horizontaltabs.php
@@ -1,82 +1,91 @@
 <?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\Utilities\ArrayHelper;
 
-// $tagId = $params->get('tag_id', '');
-$standard = ['separator', 'component', 'heading', 'url'];
-$startLevel = $params->get('startLevel');
-$endLevel = $params->get('endLevel');
-
+$id = '';
 $attributes          = [];
+
 $attributes['class'] = 'nav nav-tabs menu horizontal' . $class_sfx;
 
 if ($tagId = $params->get('tag_id', '')) {
     $attributes['id'] = $tagId;
 }
 
-
+// The menu class is deprecated. Use mod-menu instead
 ?>
 <ul <?php echo ArrayHelper::toString($attributes); ?>>
-	<?php foreach ($list as $i => &$item) { ?>
-		<?php
-   $itemParams = $item->getParams();
-			$class = 'nav-link item-' . $item->id;
+<?php foreach ($list as $i => &$item) {
+    $itemParams = $item->getParams();
+    $class      = 'nav-item item-' . $item->id;
 
-			if ($item->id == $default_id) {
-				$class .= ' default';
-			}
+    if ($item->id == $default_id) {
+        $class .= ' default';
+    }
 
-			if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id)) {
-				$class .= ' current';
-			}
+    if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id)) {
+        $class .= ' current';
+    }
 
-			if (in_array($item->id, $path)) {
-				$class .= ' active';
-			} elseif ($item->type === 'alias') {
-				$aliasToId = $itemParams->get('aliasoptions');
+    if (in_array($item->id, $path)) {
+        $class .= ' active';
+    } elseif ($item->type === 'alias') {
+        $aliasToId = $itemParams->get('aliasoptions');
 
-				if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
-					$class .= ' active';
-				}
-				elseif (in_array($aliasToId, $path))
-				{
-					$class .= ' alias-parent-active';
-				}
-			}
+        if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+            $class .= ' active';
+        } elseif (in_array($aliasToId, $path)) {
+            $class .= ' alias-parent-active';
+        }
+    }
 
-			if ($item->type === 'separator') {
-				$class .= ' divider';
-			}
+    if ($item->type === 'separator') {
+        $class .= ' divider';
+    }
 
-			if ($item->deeper) {
-				$class .= ' deeper';
-			}
+    if ($item->deeper) {
+        $class .= ' deeper';
+    }
 
-			if ($item->parent) {
-				$class .= ' parent';
-			}
-		?>
-		
-		<?php if ($item->deeper) { ?>
-			<div class="btn-group">
-		<?php } ?>
+    if ($item->parent) {
+        $class .= ' parent';
+    }
 
-			<?php if (in_array($item->type, $standard)) { ?>
-				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'horizontaltabs_' . $item->type)); ?>
-			<?php } else { ?>
-				<?php require(JModuleHelper::getLayoutPath('mod_menu', 'horizontaltabs_url')); ?>
-			<?php } ?>
+    echo '<li class="' . $class . '">';
 
-			<?php if ($item->deeper) { ?>
-				<ul class="dropdown-menu">
-			<?php } ?>
+    switch ($item->type) :
+        case 'separator':
+        case 'component':
+        case 'heading':
+        case 'url':
+            require ModuleHelper::getLayoutPath('mod_menu', 'horizontaltabs_' . $item->type);
+            break;
 
-			<?php if ($item->shallower) { ?>
-			</ul>
-			</div>
-			<?php } ?>
-	<?php } ?>
+        default:
+            require ModuleHelper::getLayoutPath('mod_menu', 'horizontaltabs_url');
+            break;
+    endswitch;
 
-</ul>
+    // The next item is deeper.
+    if ($item->deeper) {
+        echo '<ul class="dropdown-menu">';
+    } elseif ($item->shallower) {
+        // The next item is shallower.
+        echo '</li>';
+        echo str_repeat('</ul></li>', $item->level_diff);
+    } else {
+        // The next item is on the same level.
+        echo '</li>';
+    }
+}
+?></ul>
--- atomic/html/mod_menu/horizontaltabs_component.php
+++ atomic/html/mod_menu/horizontaltabs_component.php
@@ -1,74 +1,95 @@
 <?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') or die;
 
-$attributes = array();
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\Filter\OutputFilter;
 
-if ($item->anchor_title) {
-	$attributes['title'] = $item->anchor_title;
-}
-
+$attributes = [];
 $attributes['class'] = '';
 
-if ($item->parent && $item->level <= $startLevel) {
-	$attributes['class'] .= 'btn dropdown-toggle';
-	$attributes['data-bs-toggle'] = 'dropdown';
-	//$attributes['aria-haspopup'] = 'true';
-	$attributes['aria-expanded'] = 'false';
-
-	// If this is a parent link, we need to ensure that the link goes nowhere
-	$item->flink = '#';
+if ($item->anchor_title) {
+    $attributes['title'] = $item->anchor_title;
 }
 
-if ($item->level > $startLevel) {
-	$attributes['class'] = ' dropdown-item';
-}
-
-$attributes['class'] .= ' ' . $class;
-
-// Options from menu
 if ($item->anchor_css) {
-	$attributes['class'] .= ' ' . $item->anchor_css;
+    $attributes['class'] = $item->anchor_css;
 }
 
 if ($item->anchor_rel) {
-	$attributes['rel'] = $item->anchor_rel;
+    $attributes['rel'] = $item->anchor_rel;
+}
+
+if ($item->id == $active_id) {
+    $attributes['aria-current'] = 'location';
+
+    if ($item->current) {
+        $attributes['aria-current'] = 'page';
+    }
 }
 
 $linktype = $item->title;
 
-if ($item->menu_image) {
-	if ($item->menu_image_css) {
-		$image_attributes['class'] = $item->menu_image_css;
-		$linktype = JHtml::_('image', $item->menu_image, $item->title, $image_attributes);
-	} else {
-		$linktype = JHtml::_('image', $item->menu_image, $item->title);
-	}
+if ($item->menu_icon) {
+    // The link is an icon
+    if ($itemParams->get('menu_text', 1)) {
+        // If the link text is to be displayed, the icon is added with aria-hidden
+        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
+    } else {
+        // If the icon itself is the link, it needs a visually hidden text
+        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
+    }
+} elseif ($item->menu_image) {
+    // The link is an image, maybe with its own class
+    $image_attributes = [];
 
-	if ($item->params->get('menu_text', 1)) {
-		$linktype .= '<span class="image-title">' . $item->title . '</span>';
-	}
+    if ($item->menu_image_css) {
+        $image_attributes['class'] = $item->menu_image_css;
+    }
+
+    $linktype = HTMLHelper::_('image', $item->menu_image, $item->title, $image_attributes);
+
+    if ($itemParams->get('menu_text', 1)) {
+        $linktype .= '<span class="image-title">' . $item->title . '</span>';
+    }
 }
 
 if ($item->browserNav == 1) {
-	$attributes['target'] = '_blank';
+    $attributes['target'] = '_blank';
 } elseif ($item->browserNav == 2) {
-	$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes';
+    $options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes';
 
-	$attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
+    $attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
 }
-if ($item->parent && $item->level <= $startLevel) {
-    echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
+
+if ( $item->level > 1) {
+    $attributes['class'] .= ' dropdown-item';
+} else {
+    $attributes['class'] .= ' nav-link';
 }
-else
-{  
-   if($item->level=='1')
-   {
-     $attributes['class']='btn first-heading';
-    echo "<div class='btn-group'>".JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes)."</div>";
-   }
-  else
-  {
-    echo '<li class="nav-item">'.JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes)."</li>";
-  }
+
+if ($item->deeper) {
+    $attributes['class'] .= ' dropdown-toggle';
 }
-//echo "<li id='test1'>".JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes)."</li>";
+
+if (in_array($item->id, $path)) {
+    $attributes['class'] .= ' active';
+} elseif ($item->type === 'alias') {
+    $aliasToId = $itemParams->get('aliasoptions');
+
+    if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+        $attributes['class'] .= ' active';
+    } elseif (in_array($aliasToId, $path)) {
+        $attributes['class'] .= ' alias-parent-active';
+    }
+}
+
+echo HTMLHelper::_('link', OutputFilter::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
